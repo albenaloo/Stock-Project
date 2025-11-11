9a8d26ba8010924cf37b3d762bc490dd7a765558
lindaaaaen <hanxien24@gmail.com>
2025-11-10 21:47:57 -0500

    method

--- java/src/Entity/EarningsRecord.java
+++ java/src/Entity/EarningsRecord.java
@@ -4,6 +4,7 @@ public final class EarningsRecord {
     public final String symbol, period;
     public final int year, quarter;
     public final double estimate, actual, surprise, surprisePercent;
+
     public EarningsRecord(String symbol, String period, int year, int quarter) {
         this.symbol = symbol;
         this.period = period;
@@ -13,5 +14,36 @@ public final class EarningsRecord {
         this.actual = 0.0;
         this.surprise = 0.0;
         this.surprisePercent = 0.0;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public int getYear() {
+            return year;
+    }
+    public int getQuarter() {
+        return quarter;
+    }
+
+    public double getEstimate() {
+        return estimate;
+    }
+
+    public double getActual() {
+        return actual;
+    }
+
+    public double getSurprise() {
+        return surprise;
+    }
+
+    public double getSurprisePercent() {
+        return surprisePercent;
     }
 }
