eee17968b394cc52f745b3f21898cd07784a681f
lindaaaaen <hanxien24@gmail.com>
2025-11-12 13:33:52 -0500

    fix code

--- java/src/Entity/EarningsRecord.java
+++ java/src/Entity/EarningsRecord.java
@@ -5,15 +5,15 @@ public final class EarningsRecord {
     public final int year, quarter;
     public final double estimate, actual, surprise, surprisePercent;
 
-    public EarningsRecord(String symbol, String period, int year, int quarter) {
+    public EarningsRecord(String symbol, String period, int year, int quarter, double estimate, double actual, double surprise, double surprisePercent) {
         this.symbol = symbol;
         this.period = period;
         this.year = year;
         this.quarter = quarter;
-        this.estimate = 0.0;
-        this.actual = 0.0;
-        this.surprise = 0.0;
-        this.surprisePercent = 0.0;
+        this.estimate = estimate;
+        this.actual = actual;
+        this.surprise = surprise;
+        this.surprisePercent = surprisePercent;
     }
 
     public String getSymbol() {
