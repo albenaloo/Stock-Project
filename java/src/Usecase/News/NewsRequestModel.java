4579bf3eb98b7af407a858d951247e5f6b490e23
samario <samario.liu@mail.utoronto.ca>
2025-11-16 12:54:54 -0500

    NewsRequestModel implemented

--- java/src/Usecase/News/NewsRequestModel.java
+++ java/src/Usecase/News/NewsRequestModel.java
@@ -1,4 +1,18 @@
 package Usecase.News;
 
 public class NewsRequestModel {
+
+    private final String symbol;   // null -> market news
+
+    public NewsRequestModel() {
+        this.symbol = null;
+    }
+
+    public NewsRequestModel(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
