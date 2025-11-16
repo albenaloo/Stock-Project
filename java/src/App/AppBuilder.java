1396fd5bdcc535195739c3ea2c20ef960c1f4a3d
Ale Benavides Loo <albenaloo27@gmail.com>
2025-11-16 14:09:48 -0500

    Usre and UserFactory Added

---
+++ java/src/App/AppBuilder.java
@@ -0,0 +1,5 @@
+package App;
+
+import DataAccess.FileUserDataAcessObject;
+import Entity.UserFactory;
+
---
+++ java/src/Entity/UserFactory.java
@@ -0,0 +1,11 @@
+package Entity;
+
+/**
+ * Factory for creating CommonUser objects.
+ */
+public class UserFactory {
+
+    public User create(String name, String password) {
+        return new User(name, password);
+    }
+}
