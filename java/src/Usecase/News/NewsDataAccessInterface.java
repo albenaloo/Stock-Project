a3da4a6547dc89c37246991439aaa8d292d4460a
samario <samario.liu@mail.utoronto.ca>
2025-11-16 12:58:57 -0500

    NewsDataAccessInterface implemented

--- java/src/Usecase/News/NewsDataAccessInterface.java
+++ java/src/Usecase/News/NewsDataAccessInterface.java
@@ -1,4 +1,15 @@
 package Usecase.News;
 
-public class NewsDataAccessInterface {
+import Entity.NewsArticle;
+import java.util.List;
+
+public interface NewsDataAccessInterface {
+
+    // /news?category=general
+    List<NewsArticle> loadMarketNews() throws Exception;
+
+    // /company-news?symbol=...&from=...&to=...
+    List<NewsArticle> loadCompanyNews(String symbol,
+                                      String fromDate,
+                                      String toDate) throws Exception;
 }
