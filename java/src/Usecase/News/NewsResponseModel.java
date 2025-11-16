fc15ed56c38168b9e654f083b6a37e81bf07b787
samario <samario.liu@mail.utoronto.ca>
2025-11-16 12:54:36 -0500

    NewsResponseModel implemented

--- java/src/Usecase/News/NewsResponseModel.java
+++ java/src/Usecase/News/NewsResponseModel.java
@@ -1,4 +1,17 @@
 package Usecase.News;
 
+import Entity.NewsArticle;
+import java.util.List;
+
 public class NewsResponseModel {
+
+    private final List<NewsArticle> articles;
+
+    public NewsResponseModel(List<NewsArticle> articles) {
+        this.articles = articles;
+    }
+
+    public List<NewsArticle> getArticles() {
+        return articles;
+    }
 }
