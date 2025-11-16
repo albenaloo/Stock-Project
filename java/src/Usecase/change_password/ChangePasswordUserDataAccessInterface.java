40978432f30a592540a08ccaa38e76f35ed7c987
Ale Benavides Loo <albenaloo27@gmail.com>
2025-11-16 14:19:20 -0500

    Change_Password added

---
+++ java/src/Usecase/change_password/ChangePasswordUserDataAccessInterface.java
@@ -0,0 +1,15 @@
+package Usecase.change_password;
+
+import Entity.User;
+
+/**
+ * The DAO interface for the Change Password Use Case.
+ */
+public interface ChangePasswordUserDataAccessInterface {
+
+    /**
+     * Updates the system to record this user's password.
+     * @param user the user whose password is to be updated
+     */
+    void changePassword(User user);
+}
