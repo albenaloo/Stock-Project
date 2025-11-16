d6337431e0e48de1b34568eaf175e389a76ea9d5
Ale Benavides Loo <albenaloo27@gmail.com>
2025-11-16 17:04:30 -0500

    Added Signup Usecase

---
+++ java/src/Usecase/signup/SignupOutputData.java
@@ -0,0 +1,18 @@
+package Usecase.signup;
+
+/**
+ * Output Data for the Signup Use Case.
+ */
+public class SignupOutputData {
+
+    private final String username;
+
+    public SignupOutputData(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
